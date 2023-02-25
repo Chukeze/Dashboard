526472339ef40215f45ef433c96b1b1bc4c9407d
Chukeze <enile12@gmail.com>
2023-02-24 18:51:29 -0600

    Added example dashboard carousel

---
+++ assets/components/Admin/script.js
@@ -0,0 +1,12 @@
+const chartOne = {
+  name: 'Stocks',
+  x_coords: [0, 1, 2, 3, 4, 5, 6],
+  y_coords: [0, 100, 200, 300, 400, 500],
+};
+
+const chartTwo = {
+  name: 'Wages',
+  x_coords: [0, 1, 2, 3, 4, 5, 6],
+  y_coords: [0, 100, 200, 300, 400, 500],
+}
+chartTwo, chartOne;
